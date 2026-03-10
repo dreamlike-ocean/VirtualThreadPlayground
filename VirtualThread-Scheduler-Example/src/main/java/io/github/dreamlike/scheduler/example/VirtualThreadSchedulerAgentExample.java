09e4bef1cb242e6f06505490ddfb255ec99aada9
dreamlike-ocean <dreamlike.vertx@gmail.com>
2026-03-10 23:57:41 +0800

    1 修复debug时间太长的问题

--- VirtualThread-Scheduler-Example/src/main/java/io/github/dreamlike/scheduler/example/VirtualThreadSchedulerAgentExample.java
+++ VirtualThread-Scheduler-Example/src/main/java/io/github/dreamlike/scheduler/example/VirtualThreadSchedulerAgentExample.java
@@ -1,14 +1,12 @@
 package io.github.dreamlike.scheduler.example;
 
-import com.sun.tools.attach.VirtualMachine;
-
-import java.lang.management.ManagementFactory;
-import java.nio.file.Paths;
+import java.util.Scanner;
 
 public final class VirtualThreadSchedulerAgentExample {
 
     public static void main(String[] args) throws ClassNotFoundException {
        Class.forName("sun.nio.ch.KQueuePoller");
+       new Scanner(System.in).nextLine();
     }
 
 }
